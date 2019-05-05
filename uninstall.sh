8a7eca51a25d127c33bf7afab4d5c9f10f38a167
Haridas N <haridas.nss@gmail.com>
2019-05-05 15:52:37 +0530

    Updated the uninstal.sh script with tmux path change.

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,8 @@
-read -p "Deleteing these files - ~/.vim ~/.tmux.conf  ~/Dotfiles ~/Dotfiles-master (y/n): " confirm
+read -p "Deleteing these files - ~/.vim ~/.tmux.conf  ~/.tmux ~/Dotfiles ~/Dotfiles-master (y/n): " confirm
 
 if [[ $confirm == y ]]
 then
-    rm -rf ~/.vim ~/.tmux.conf ~/Dotfiles-master
+    rm -rf ~/.vim ~/.tmux ~/.tmux.conf ~/Dotfiles-master
     echo "Uninstalled the dotfile setup from your machine !"
 else
     echo "Exited without cleaning. !"
