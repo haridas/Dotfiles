5824515ce0da16cd5614c84842568690ac819f9a
Haridas N <haridas.nss@gmail.com>
2020-10-05 14:05:40 +0530

    Added pylint also for the python linting.

--- mac-setup.sh
+++ mac-setup.sh
@@ -7,7 +7,7 @@ test -x brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com
 brew install python3 tmux git wget
 
 # for python linting.
-pip3 install flak8
+pip3 install flake8 pylint
 
 # Other parts are same as the linux setup.
 wget -q https://raw.githubusercontent.com/haridas/Dotfiles/master/linux-setup.sh -O - | bash
